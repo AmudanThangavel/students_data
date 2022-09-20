c281026e0d172b25592d9e1a5e94e02f17873234
AmudanThangavel <amudanthangavel@gmail.com>
2022-09-20 12:12:48 +0530

    added column

--- student_scores/accounts/admin.py
+++ student_scores/accounts/admin.py
@@ -3,4 +3,10 @@ from accounts.models import students_data
 # Register your models here.
 
 
-admin.site.register(students_data)
+class StudentsDataAdmin(students_data):
+    list_filter = ('roll_no', 'team_no', 'score')
+    list_display = ('name', 'roll_no', 'score', 'team_no')
+
+
+# admin.site.register(students_data)
+admin.site.register(students_data, StudentsDataAdmin)
