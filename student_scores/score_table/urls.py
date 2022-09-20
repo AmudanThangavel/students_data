678c1feb4d18cbd14b691bfd02dc48914abaae6f
AmudanThangavel <amudanthangavel@gmail.com>
2022-09-20 15:30:29 +0530

    integrated database table with html
    some error in rendering the table

--- student_scores/score_table/urls.py
+++ student_scores/score_table/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path, include
-from .views import index
+from .views import *
 
 urlpatterns = [
-    path("test/",index),
+    path("test/", index),
+    path('', ScoreTable)
 ]
