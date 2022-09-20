1aa2a77da307e96733abb7c5e1ae9fd853aab1c3
AmudanThangavel <amudanthangavel@gmail.com>
2022-09-20 11:43:24 +0530

    created table and migrated

--- student_scores/accounts/admin.py
+++ student_scores/accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from accounts.models import students_data
 # Register your models here.
+
+
+admin.site.register(students_data)
--- student_scores/accounts/models.py
+++ student_scores/accounts/models.py
@@ -1,11 +1,10 @@
 from django.db import models
-from django.db import models
 # Create your models here.
 
 
-class students_data(models.Models):
+class students_data(models.Model):
     name = models.CharField(max_length=30)
     roll_no = models.CharField(max_length=10)
     department = models.CharField(max_length=35)
-    score = models.DecimalField()
+    score = models.DecimalField(max_digits=10, decimal_places=3)
     team_no = models.IntegerField()
