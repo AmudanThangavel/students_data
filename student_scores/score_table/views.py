4109ecad3ee2eabc88ec7eebd97b65144bb4c656
Anandhan <anandhangithub@gmail.com>
2022-09-22 11:30:33 +0530

    final wedpage with updated time and date

--- student_scores/score_table/views.py
+++ student_scores/score_table/views.py
@@ -1,3 +1,5 @@
+import datetime
+from turtle import update
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from accounts.models import students_data
@@ -21,6 +23,7 @@ from collections import defaultdict
 from .views import *
 from django.templatetags.static import static
 import heapq
+import pytz
 
 # imports for rendering the table
 from accounts.models import students_data
@@ -70,52 +73,56 @@ def index(request):
 
 
 def run_selenium():
-    try:
-        module_dir = os.path.dirname(__file__)  # get current directory
-        # full path to text.
-        chromedriver = os.path.join(module_dir, 'static/chromedriver.exe')
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('excludeSwitches', ['enable-logging'])
-        driver = webdriver.Chrome(
-            options=options, executable_path=chromedriver)
-        driver.minimize_window()
-        DriverWait(driver, "https://www.interviewbit.com/users/sign_in/")
-        driver.find_element(
-            'xpath', '//input[@id = "user_email_field"]').send_keys("cdczoom@kpriet.ac.in")
-        driver.find_element(
-            'xpath', '//input[@id = "user_password_field"]').send_keys("12345678")
-        driver.find_element(
-            'xpath', '//input[@data-gtm-element = "login"]').click()
-        id1, score = [], []
-        for page in range(1, 13):
-            DriverWait(
-                driver, "https://www.interviewbit.com/leaderboard/?followers=1&page="+str(page))
-            for i in driver.find_elements(By.TAG_NAME, "a"):
+    th=0
+    while(th!=1):
+        try:
+            module_dir = os.path.dirname(__file__)  # get current directory
+            # full path to text.
+            chromedriver = os.path.join(module_dir, 'static/chromedriver.exe')
+            options = webdriver.ChromeOptions()
+            options.add_experimental_option('excludeSwitches', ['enable-logging'])
+            driver = webdriver.Chrome(
+                options=options, executable_path=chromedriver)
+            driver.minimize_window()
+            DriverWait(driver, "https://www.interviewbit.com/users/sign_in/")
+            driver.find_element(
+                'xpath', '//input[@id = "user_email_field"]').send_keys("cdczoom@kpriet.ac.in")
+            driver.find_element(
+                'xpath', '//input[@id = "user_password_field"]').send_keys("12345678")
+            driver.find_element(
+                'xpath', '//input[@data-gtm-element = "login"]').click()
+            id1, score = [], []
+            for page in range(1, 13):
+                DriverWait(
+                    driver, "https://www.interviewbit.com/leaderboard/?followers=1&page="+str(page))
+                for i in driver.find_elements(By.TAG_NAME, "a"):
+                    try:
+                        if str(i.get_attribute('href')).count('profile') > 0 and str(i.get_attribute('href')).count('cdc-zoom') == 0:
+                            #                 print(i.get_attribute('href'),i.text)
+                            id1.append(i.get_attribute('href')[
+                                    i.get_attribute('href').rindex('/')+1:])
+                    except:
+                        pass
+                for i in driver.find_elements(By.TAG_NAME, "tr"):
+                    try:
+                        if i.text != 'Rank User Level Streak Score':
+                            score.append(int(i.text[i.text.rindex(' ')+1:]))
+                    except:
+                        pass
+            print(id1, score)
+            for i, j in zip(id1, score):
                 try:
-                    if str(i.get_attribute('href')).count('profile') > 0 and str(i.get_attribute('href')).count('cdc-zoom') == 0:
-                        #                 print(i.get_attribute('href'),i.text)
-                        id1.append(i.get_attribute('href')[
-                                   i.get_attribute('href').rindex('/')+1:])
-                except:
+                    obj = students_data.objects.get(roll_no=i.upper())
+                    obj.score = j
+                    obj.name = obj.name.upper()
+                    obj.updated_at = datetime.datetime.now()
+                    obj.save()
                     pass
-            for i in driver.find_elements(By.TAG_NAME, "tr"):
-                try:
-                    if i.text != 'Rank User Level Streak Score':
-                        score.append(int(i.text[i.text.rindex(' ')+1:]))
                 except:
-                    pass
-        print(id1, score)
-        for i, j in zip(id1, score):
-            try:
-                obj = students_data.objects.get(roll_no=i.upper())
-                obj.score = j
-                obj.name = obj.name.upper()
-                obj.save()
-                pass
-            except:
-                print(i)
-    except:
-        print(sys.exc_info())
+                    print(i)
+        except:
+            print(sys.exc_info())
+        time.sleep(600)
 
 
 def Fetch(request):
@@ -133,6 +140,7 @@ def ScoreTable(request):
     top10team = []
     top10score = []
     teamScore = students_data.objects.all()
+    
     temp = students_data.objects.all()
     m = max([i.team_no for i in temp])
     sc = list(set([j.score for j in temp]))
@@ -143,7 +151,7 @@ def ScoreTable(request):
         scr = []
         for iter in b:
             scr.append(int(iter.score))
-            l.append((iter.name)+ "-" +iter.department)
+            l.append((iter.name).ljust(30)+(iter.department))
         top10mem.append(l)
         top10score.append(scr)
     count = {}
@@ -153,7 +161,7 @@ def ScoreTable(request):
         sum = 0
         for j in fl:
             sum += int(j.score)
-        d[i] = sum/c
+        d[i] = round(sum/c,2)
         count[i] = c
     a = dict(reversed(sorted(d.items(), key=lambda item: item[1])))
 
@@ -168,4 +176,14 @@ def ScoreTable(request):
     print(top10mem)
     print(top10score)
     rng = [i for i in range(1,11)]
-    return render(request, 'scores.html', {"students_data": teamScore, "rank": a, "count": count, "top10team": top10team, "top10per": top10mem, "top10score": top10score, "range": rng})
+
+    #Time
+    updatedTime = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
+    # uptimeobj = students_data.objects.get(roll_no = "20EC023")
+    # updatedTime = uptimeobj.updated_at
+    print(str(updatedTime))
+    # timestr = str(updatedTime.day) + "-" + str(updatedTime.month) + "Time : " + str(updatedTime.hour) + ":" + str(updatedTime.minute )
+    timestr = "Updated at " +  updatedTime.strftime('%Y:%m:%d %H:%M:%S')
+    # print(times)
+
+    return render(request, 'scores.html', {"students_data": teamScore, "rank": a, "count": count, "top10team": top10team, "top10per": top10mem, "top10score": top10score, "range": rng, "updateTime" : timestr})
